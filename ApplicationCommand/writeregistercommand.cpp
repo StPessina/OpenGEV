fe1ab732e529861c0e041b10f664d432e613a327
StPessina <st.pessina@gmail.com>
2015-11-16 16:52:34 +0100

    Init write command, fix sanity check in conversion utils

---
+++ ApplicationCommand/writeregistercommand.cpp
@@ -0,0 +1,5 @@
+#include "writeregistercommand.h"
+
+WriteRegisterCommand::WriteRegisterCommand()
+{
+}
---
+++ ApplicationCommand/writeregistercommand.h
@@ -0,0 +1,10 @@
+#ifndef WRITEREGISTERCOMMAND_H
+#define WRITEREGISTERCOMMAND_H
+
+class WriteRegisterCommand
+{
+public:
+    WriteRegisterCommand();
+};
+
+#endif // WRITEREGISTERCOMMAND_H
